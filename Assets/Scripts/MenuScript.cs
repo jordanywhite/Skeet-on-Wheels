65e0872e34ab1d9c0c22f15e76ea7bc4696cce56
Murray Studio <sushishamus@gmail.com>
2016-02-28 14:12:44 -0800

    added menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+    public Button startBtn, exitBtn;
+
+	// Use this for initialization
+	void Start () {
+        //Screen.SetResolution(800, 780, false);
+        startBtn = startBtn.GetComponent<Button>();
+        exitBtn = exitBtn.GetComponent<Button>();
+
+        //startBtn.onClick.AddListener(() => { Play(); });
+        //exitBtn.onClick.AddListener(() => { ExitGame(); });
+
+        //PlayerPrefs.DeleteAll();
+    }
+
+    public void Play()
+    {
+        //SceneManager.LoadScene("Main");
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
