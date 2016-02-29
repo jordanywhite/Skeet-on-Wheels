450ff66a1e21a913e6166a169d493e8de9dc2c20
Lucas Burns <burnsl17@up.edu>
2016-02-29 12:59:14 -0800

    Actually added sounds and stuff this time.

---
+++ Assets/Scripts/audioScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioScript : MonoBehaviour {
+	private GameObject player;
+	private AudioSource audio;
+	private bool hasPlayed;
+	public int triggerDistance = 30;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		audio = this.gameObject.GetComponent <AudioSource> ();
+		audio.playOnAwake = false;
+		hasPlayed = false;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (player.transform.position, this.gameObject.transform.position) <= triggerDistance && hasPlayed == false)
+		{
+			audio.Play ();
+			hasPlayed = true;
+		}
+	}
+}
